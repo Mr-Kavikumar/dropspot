70478a7c3399913ee87f4dbeb48e35e648ff1355
Kavikumar-C <ckavikumar@gmail.com>
2023-05-04 15:13:58 +0530

    Initial Commit

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import  {Text,View} from 'react-native';
+import React from 'react'
+import { NativeWindStyleSheet } from 'nativewind';
+
+export const HomeScreen = () => {
+  return (
+      <Text classname='bg-red-800'>HomeScreen</Text>
+  )
+}
